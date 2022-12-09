2c4b73202b0bf7c70992f60f087a83d203afde2c
DEVELoper <syabblogger@gmail.com>
2022-12-09 16:30:27 +0500

    Prices and Discount are Added with Different Panels of items with Final Bill Showing

--- main.py
+++ main.py
@@ -1,6 +1,16 @@
 print("Restaurant Project using Python Programming\n Language For Noobs")
 
+
 # TO Display all manu at once
+
+class AllVariablesDeclaredHere:
+
+    ChickenPolao = 1500
+    ChickenTikka = 2000
+    Mutton = 2500
+    Haleem = 700
+    Rice = 1800
+
 
 class ManuItemsToDisplay:
     def displayItems(self):
@@ -10,23 +20,64 @@ class ManuItemsToDisplay:
         print("4: Haleem")
         print("5: Rice")
 
-class ManuPanels:
+
+class ManuPanels():
+
     def ChickenPolaoPanel(self):
-        print("1: Chicken Polao panel")
+        variables = AllVariablesDeclaredHere()
+        print("********* || __Chicken Polao panel__ || *********")
+        print(" Chicken Polao Selected : Price is ", variables.ChickenPolao)
+        quantityInNumbers = int(input("Enter Quantity: "))
+        priceWithDis = (variables.ChickenPolao/100)*2
+        billWithDis = quantityInNumbers * variables.ChickenPolao
+        FinalBillWithDis = priceWithDis - billWithDis
+        print(" You Bill is: " ,FinalBillWithDis)
+
     def ChicekTikkaPanel(self):
-        print("2: Chicken Tikka panel")
+        variables = AllVariablesDeclaredHere()
+        print("********* || __Chicken Tikka panel__ || *********")
+        print(" Chicken Tikka Selected : Price is ", variables.ChickenTikka)
+        quantityInNumbers = int(input("Enter Quantity: "))
+        priceWithDis = (variables.ChickenTikka / 100) * 2
+        billWithDis = quantityInNumbers * variables.ChickenTikka
+        FinalBillWithDis = priceWithDis - billWithDis
+        print(" You Bill is: " ,FinalBillWithDis)
+
     def MuttonPanel(self):
-        print("3: Mutton panel")
+        variables = AllVariablesDeclaredHere()
+        print("********* || __Mutton Panel panel__ || *********")
+        print(" Mutton Selected : Price is ", variables.Mutton)
+        quantityInNumbers = int(input("Enter Quantity: "))
+        priceWithDis = (variables.Mutton / 100) * 2
+        billWithDis = quantityInNumbers * variables.Mutton
+        FinalBillWithDis = priceWithDis - billWithDis
+        print(" You Bill is: " ,FinalBillWithDis)
+
     def HaleemPanel(self):
-        print("4: Haleem panel")
+        variables = AllVariablesDeclaredHere()
+        print("********* || __Haleem panel__ || *********")
+        print(" Haleem Selected : Price is ", variables.Haleem)
+        quantityInNumbers = int(input("Enter Quantity: "))
+        priceWithDis = (variables.Haleem / 100) * 2
+        billWithDis = quantityInNumbers * variables.Haleem
+        FinalBillWithDis = priceWithDis - billWithDis
+        print(" You Bill is: " ,FinalBillWithDis)
+
     def RicePanel(self):
-        print("5: Rice panel")
+        variables = AllVariablesDeclaredHere()
+        print("********* || __Rice panel__ || *********")
+        print(" Rice Selected : Price is ", variables.Rice)
+        quantityInNumbers = int(input("Enter Quantity: "))
+        priceWithDis = (variables.Rice / 100) * 2
+        billWithDis = quantityInNumbers * variables.Rice
+        FinalBillWithDis = priceWithDis - billWithDis
+        print(" You Bill is: " ,FinalBillWithDis)
 
 
 class GettingOrder:
     def GettingOrderFromUser(self):
         panel = ManuPanels()
-        orderInputForOrder = input("Enter you Desire Input: ")
+        orderInputForOrder = int(input("Enter you Desire Input: "))
         if orderInputForOrder == 1:
             panel.ChickenPolaoPanel()
         elif orderInputForOrder == 2:
@@ -39,7 +90,7 @@ class GettingOrder:
             panel.RicePanel()
 
 
-#----------------------------------------------------------------
+# ----------------------------------------------------------------
 # Main Method
 # we should call alla the methods here
 objectForItemsManu = ManuItemsToDisplay()
